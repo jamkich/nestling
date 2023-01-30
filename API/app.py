03b1263bf3655f5e02a416ee5412cc1832a3c497
Jakub <benqumichalik3@gmail.com>
2023-01-30 14:35:32 +0100

    Add some react stuff

--- API/app.py
+++ API/app.py
@@ -5,12 +5,10 @@ import requests
 app = Flask(__name__)
 CORS(app)
 
-
 def get_data():
     api = requests.get('https://api.kanye.rest')
     api = api.json()
     return api['quote']
-
 
 @app.route('/')
 def quote():
