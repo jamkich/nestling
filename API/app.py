35a18f240021134fa01bf4e6a980521937880168
Jakub <jakubnagadowski123@gmail.com>
2023-01-30 13:20:11 +0100

    Added cross-origin and api call

---
+++ API/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_cors import CORS
+import requests
+
+app = Flask(__name__)
+CORS(app)
+
+
+api = requests.get('https://api.kanye.rest')
+
+
+@app.route('/')
+def hello():
+    return "Hello WORLD!"
+
+
